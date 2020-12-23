d54d54f5053ecc61761078038d9448e28f06959e
Beyza Nur <beyza16nur@gmail.com>
2020-12-23 23:09:37 +0300

    ScriptAdd
    Player ve Coin Scripti eklendi.

---
+++ Assets/Scripts/CoinMaker.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoinMaker : MonoBehaviour
+{
+    
+    public float angle = 10;
+    public GameObject CoinPosition;
+    public GameObject CoinPrefab;
+
+    void Start()
+    {
+        var CoinParent = new GameObject();
+        CoinParent.name = "CoinParent";
+        for (int i = 0; i < (360/angle); i++)
+        {
+            this.transform.rotation = Quaternion.Euler(0, angle*i, 0);
+            Instantiate(CoinPrefab, CoinPosition.transform.position, Quaternion.identity, CoinParent.transform);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/PlayerManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerManager : MonoBehaviour
+{
+    bool isReverse = false;
+    float angle = 0;
+    public float speed = 60;
+
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        if(isReverse == true)
+        {
+            this.transform.rotation = Quaternion.Euler(0, angle, 0);
+            angle -= Time.deltaTime * speed;
+        }
+        else
+        {
+            this.transform.rotation = Quaternion.Euler(0, angle, 0);
+            angle += Time.deltaTime * speed;
+        }
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            isReverse = !isReverse;
+        }
+    }
+}
